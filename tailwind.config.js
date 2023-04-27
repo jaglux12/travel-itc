adb2e26c76743c63fdcb9573f493ed9c1085c0a3
jaglux12 <gonzalopez239@gmail.com>
2023-04-26 22:26:09 -0300

    add recomendations section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,25 @@ module.exports = {
   theme: {
     extend: {
 
-      backgroundColors: theme => ({
-        ...theme('colors'),
-        'red':'#CC2D4A',
-        'green':'#8FA206',
-        'blue':'#61AEC9'
-      }),
+      colors: {
+        'red-travel':'#CC2D4A',
+        'green-travel':'#8FA206',
+        'blue-travel':'#61AEC9'
+      },
 
       textColor:{
         'red-travel':'#CC2D4A',
-        'green':'#8FA206',
+        'green-trabe':'#8FA206',
         'blue':'#61AEC9'
+      },
+
+      backgroundImage: {
+        'iguazu':'url(https://i.postimg.cc/wMf9bKLW/Any-Conv-com-puerto-iguazu.webp)'
+      },
+
+      spacing: {
+        'width-card-rec':'154px',
+        'height-card-rec':'215px'
       }
 
 
