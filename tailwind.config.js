62143422f18c0769913de5a5bfb2c44ce21e6d99
jaglux12 <gonzalopez239@gmail.com>
2023-04-28 17:10:23 -0300

    Proyecto terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,11 @@ module.exports = {
         'isla':'url(https://i.postimg.cc/9MBmZwMH/Any-Conv-com-isla92.webp)',
         'cafayate':'url(https://i.postimg.cc/W1Q1vPKC/Any-Conv-com-cafayate.webp)',
         'calafate':'url(https://i.postimg.cc/C10MRsw-4/Any-Conv-com-calafate.webp)',
-        'bariloche':'url(https://i.postimg.cc/BZgb1kLP/Any-Conv-com-800x500px-Bariloche2-A.webp)'
+        'bariloche':'url(https://i.postimg.cc/BZgb1kLP/Any-Conv-com-800x500px-Bariloche2-A.webp)',
+        'mendoza':'url(https://i.postimg.cc/WpXp4ZCM/Any-Conv-com-mendoza.webp)',
+        'cordoba':'url(https://i.postimg.cc/N0mfGzvM/Any-Conv-com-cordob.webp)',
+        'madryn':'url(https://i.postimg.cc/JzyzvhTj/Any-Conv-com-madry.webp)',
+        'chocon':'url(https://i.postimg.cc/wM0B8R8Q/Any-Conv-com-chocon-WEB-770x513.webp)'
       },
 
       spacing: {
@@ -31,7 +35,10 @@ module.exports = {
         'height-card-rec':'215px',
         'height-card-full':'402px',
         'width-card-full':'334px',
-        'height-header':'73px'
+        'height-header':'73px',
+        'wheader-title-tablet':'550px',
+        'height-cafayate-tb':'830px',
+        'height-caf2-tb':'403px'
       },
 
       fontFamily: {
@@ -40,6 +47,10 @@ module.exports = {
 
       screens:{
         'min-laptop':'880px'
+      },
+
+      gridAutoRows:{
+        'row-3':'minmax(0,0.5fr)'
       }
 
     },
